152356a973f3e9396bc7aa2a3f39206b92d477c8
michalmazurdev <m.mazur40@gmail.com>
2023-06-25 21:19:29 +0200

    a

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,19 @@
+import css from './Searchbar.module.css';
+
+export const Searchbar = () => (
+  <header className={css.searchbar}>
+    <form className={css.SearchForm}>
+      <button type="submit" className={css.button}>
+        <span className="button-label">Search</span>
+      </button>
+
+      <input
+        className={css.input}
+        type="text"
+        autocomplete="off"
+        autofocus
+        placeholder="Search images and photos"
+      />
+    </form>
+  </header>
+);
